7c7f6f7a0de791d40e4c269361d801c53c27573d
TANEL PÄÄRO <tpaaro@itcollege.ee>
2015-08-03 16:08:14 +0300

    nufin

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=laptopid',
-    'username' => 'laptopid',
-    'password' => 'NmGFz3G38wv7wsUK',
-    'charset' => 'utf8',
-];
